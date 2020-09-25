18d436ef52a098d7735e0aa1da6688e91702c32a
eltayeb <gaaliii@yahoo.com>
2020-09-25 11:47:38 +0200

    update-init-script

--- src/init.js
+++ src/init.js
@@ -2,3 +2,20 @@
 
 
 console.log('hello');
+
+
+import {Timer} from './timer.js'
+
+const duration = document.querySelector('#duration');
+const startStopButton = document.querySelector('.start');
+
+const pomodoroBtn = document.querySelector('#pomodoro-btn');
+const shortBreakBtn = document.querySelector('#short-break-btn');
+const longBreakBtn = document.querySelector('#long-break-btn');
+const body = document.querySelector('body');
+const timerTypeLabel = document.querySelector('#timer-type-label');
+
+const timer = new Timer( duration, startStopButton, pomodoroBtn, shortBreakBtn, longBreakBtn, body, timerTypeLabel);
+
+
+
